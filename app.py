67d089a684ee7779a7ab71ba22c4cf3ef552dff1
dertrockx <ivangrey67@gmail.com>
2019-03-06 09:41:47 +0800

    Moved models, schemas and wrapers to each of their seperate file.

--- app.py
+++ app.py
@@ -1,173 +1,20 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for
 from flask_socketio import SocketIO
-
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_, desc
-
-from flask_marshmallow import Marshmallow
-
-import hashlib
-from datetime import datetime
-
 import random, string, pprint, uuid
-
-from functools import wraps
-
 from flask_wtf import CSRFProtect
 from forms import LoginForm, SignupForm
-
-
+from models import db, User, Device, Session, Message, ActiveUser
+from schema import ma, UserSchema, DeviceSchema, SessionSchema, MessageSchema, ActiveUserSchema
+from wrappers import is_logged_in
 
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits))
-app.config['SECRET_KEY'] = 'secret'
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= True
+app.config.from_pyfile('config.py')
 
-db = SQLAlchemy(app)
+db.init_app(app)
 csrf = CSRFProtect(app)
 io = SocketIO(app)
-ma = Marshmallow(app)
-
-class User(db.Model):
-	__tablename__ = 'users'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True, nullable=False)
-	email = db.Column(db.String(80), unique=True, nullable=False)
-	password = db.Column(db.String(), nullable=False)
-	date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
-	sessions = db.relationship("Session", backref='user_sessions', lazy=True, passive_deletes=True)
-	devices = db.relationship('Device', backref='user_devices', lazy=True, passive_deletes=True)
-	messages = db.relationship('Message', backref='user_messages', lazy=True, passive_deletes=True)
-	active = db.relationship("ActiveUser", backref='user_active', lazy=True, uselist=False)
-	def __repr__(self):
-		return '<User {}>'.format(self.username)
-
-	def set_password(self, password):
-		hasher = hashlib.sha256()
-		hasher.update((password).encode('utf-8'))
-		self.password = hasher.hexdigest()
-		return self.password
-
-	def check_password(self, password):
-		hasher = hashlib.sha256()
-		hasher.update((password).encode('utf-8'))
-		password = hasher.hexdigest()
-		return (password == self.password)
-
-class Session(db.Model):
-	__tablename__ = 'sessions'
-	id = db.Column(db.Integer, primary_key=True)
-	session_id = db.Column(db.String(32), nullable=False)
-
-	user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-	user = db.relationship('User', lazy=True, backref='user_sessions', foreign_keys=[user_id])
-
-	device_id = db.Column(db.Integer, db.ForeignKey('devices.id', ondelete='CASCADE'), nullable=False)
-	device = db.relationship('Device', backref='device_session', lazy=True, uselist=False, foreign_keys=[device_id])
-
-	def __repr__(self):
-		return "<User {}\n\tID: {}\n\tSession: {}\n\tDevice: {}\n\tDevice UUID: {}\n".format(self.user.username, self.id, self.session_id, self.device.id, self.device.uuid)
-
-class Device(db.Model):
-	__tablename__ = 'devices'
-	id = db.Column(db.Integer, primary_key=True)
-
-	session_id = db.Column(db.Integer, db.ForeignKey('sessions.id', ondelete='CASCADE'))
-	session = db.relationship('Session', backref='device_session', lazy=True, uselist=False, foreign_keys=[session_id])
-
-	user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'))
-	user = db.relationship('User', backref='user_devices', lazy=True, foreign_keys=[user_id])
-
-	uuid = db.Column(db.String(32), nullable=False)
-	
-	def __repr__(self):
-		return "<User {}'s device:\n\tID: {}\n\tUUID: {}\n".format(self.user_id, self.id, self.uuid)
-
-class ActiveUser(db.Model):
-	__tablename__ = 'active_users'
-	id = db.Column(db.Integer, primary_key=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-	user = db.relationship('User', backref='user', lazy=True, uselist=False)
-
-	def __repr__(self):
-		return "<Active user: {}".format(self.user)
-
-
-class Message(db.Model):
-	__tablename__ = 'messages'
-	id = db.Column(db.Integer, primary_key=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-	user = db.relationship('User', backref='user_message', lazy=True)
-	message = db.Column(db.Text, nullable=False)
-	timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
-
-	def __repr__(self):
-		return "<User {}'s message: {}".format(self.user.username, self.message)
-
-'''
-	Model schemas
-'''
-
-
-class SessionSchema(ma.ModelSchema):
-	class Meta:
-		model = Session
-		fields = ('id', 'session_id', 'device_id',)
-
-class DeviceSchema(ma.ModelSchema):
-	
-	class Meta:
-		model = Device
-		fields = ('id', 'session', 'uuid',)
-
-class ActiveUserSchema(ma.ModelSchema):
-
-	class Meta:
-		model = ActiveUser
-		fields = ('id', 'user_id')
-
-class UserSchema(ma.ModelSchema):
-
-	class Meta:
-		fields = ('username', 'email', 'id', 'devices', 'sessions')
-		model = User
-	sessions = ma.Nested(SessionSchema, many=True)
-	devices = ma.Nested(DeviceSchema, many=True)
-
-class MessageSchema(ma.ModelSchema):
-	class Meta:
-		model = Message
-		fields = ('id', 'user_id', 'user', 'message', 'timestamp')	
-		
-	user = ma.Nested(UserSchema)
-
-'''
-class Messages(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	message = db.Column(db.Text, nullable=False)
-	user_id_from = db.Column(db.Integer, db.ForeignKey('users.id'))
-	user_from = db.relationship('Users', backref=db.backref('sent_messages'), lazy=True)
-	user_id_to = db.Column(db.Integer, db.ForeignKey('users.id'))
-	user_to = db.relationship('Users', backref=db.backref('received_messages'), lazy=True)
-	
-	def __repr__(self):
-		return '<Message {}>'.format(self.message)
-'''
-
-
-
-def is_logged_in(function):
-	@wraps(function)
-	def wrap(*args, **kwargs):
-		try:
-			if session['logged_in']:
-				return function(*args, **kwargs)
-		except KeyError:
-			flash("You must be logged-in to do that!", "error")
-			return redirect(url_for('login'))
-	return wrap
 
 
 @app.route('/')
